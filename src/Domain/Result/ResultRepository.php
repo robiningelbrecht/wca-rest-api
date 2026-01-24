2abd96af6b61b1a3b060fdb4fbe51dd69cd77780
timreyn <tim@cubingusa.org>
2026-01-24 12:21:00 -0500

    Update ResultRepository.php

--- src/Domain/Result/ResultRepository.php
+++ src/Domain/Result/ResultRepository.php
@@ -20,19 +20,19 @@ readonly class ResultRepository
         $queryBuilder = $this->connection->createQueryBuilder();
         $queryBuilder->select('r.*, rt.name as roundName, rt.final as isFinalRound, f.name as formatName')
             ->from('Results', 'r')
-            ->innerJoin('r', 'RoundTypes', 'rt', 'r.roundTypeId = rt.id')
-            ->innerJoin('r', 'Formats', 'f', 'r.formatId = f.id')
-            ->innerJoin('r', 'Competitions', 'c', 'r.competitionId = c.id')
-            ->innerJoin('r', 'Events', 'e', 'r.eventId = e.id')
-            ->andWhere('r.competitionId = :competitionId')
-            ->setParameter('competitionId', $competitionId)
+            ->innerJoin('r', 'round_types', 'rt', 'r.round_type_id = rt.id')
+            ->innerJoin('r', 'formats', 'f', 'r.format_id = f.id')
+            ->innerJoin('r', 'competitions', 'c', 'r.competition_id = c.id')
+            ->innerJoin('r', 'events', 'e', 'r.event_id = e.id')
+            ->andWhere('r.competition_id = :competition_id')
+            ->setParameter('competition_id', $competitionId)
             ->addOrderBy('e.rank', 'ASC')
             ->addOrderBy('rt.rank', 'DESC')
             ->addOrderBy('r.pos', 'ASC');
 
         if ($eventId) {
             $queryBuilder
-                ->andWhere('r.eventId = :event')
+                ->andWhere('r.event_id = :event')
                 ->setParameter('event', $eventId);
         }
 
@@ -64,13 +64,18 @@ readonly class ResultRepository
     public function findByPerson(string $personId): array
     {
         $query = '
-            SELECT r.*, rt.name as roundName, rt.final as isFinalRound, f.name as formatName
+            SELECT r.*, rt.name as round_name, rt.final as is_final_round, f.name as format_name
             FROM Results r
-            INNER JOIN RoundTypes rt ON r.roundTypeId = rt.id
-            INNER JOIN Formats f ON r.formatId = f.id
-            INNER JOIN Competitions c ON r.competitionId = c.id
-            INNER JOIN Events e ON r.eventId = e.id
-            WHERE personId = :personId
+            INNER JOIN round_types rt ON r.round_type_id = rt.id
+            INNER JOIN formats f ON r.format_id = f.id
+            INNER JOIN competitions c ON r.competition_id = c.id
+            INNER JOIN events e ON r.event_id = e.id
+            LEFT JOIN result_attempts v1 ON v1.result_id = r.id AND v1.attempt_number=1
+            LEFT JOIN result_attempts v2 ON v2.result_id = r.id AND v2.attempt_number=2
+            LEFT JOIN result_attempts v3 ON v3.result_id = r.id AND v3.attempt_number=3
+            LEFT JOIN result_attempts v4 ON v4.result_id = r.id AND v4.attempt_number=4
+            LEFT JOIN result_attempts v5 ON v5.result_id = r.id AND v5.attempt_number=5
+            WHERE person_id = :person_id
             ORDER BY c.year DESC, c.month DESC, c.day DESC, e.rank ASC, rt.rank DESC
         ';
 
@@ -87,15 +92,15 @@ readonly class ResultRepository
     private function buildResult(array $result): Result
     {
         return Result::fromState(
-            competitionId: $result['competitionId'],
-            personId: $result['personId'],
-            eventId: $result['eventId'],
-            round: $result['roundName'],
-            isFinalRound: !empty($result['isFinalRound']),
+            competitionId: $result['competition_id'],
+            personId: $result['person_id'],
+            eventId: $result['event_id'],
+            round: $result['round_name'],
+            isFinalRound: !empty($result['is_final_round']),
             position: $result['pos'],
             best: $result['best'],
             average: $result['average'],
-            format: $result['formatName'],
+            format: $result['format_name'],
             solves: [
                 $result['value1'],
                 $result['value2'],
@@ -103,8 +108,8 @@ readonly class ResultRepository
                 $result['value4'],
                 $result['value5'],
             ],
-            singleRecord: Record::tryFromMap($result['regionalSingleRecord']),
-            averageRecord: Record::tryFromMap($result['regionalAverageRecord'])
+            singleRecord: Record::tryFromMap($result['regional_single_record']),
+            averageRecord: Record::tryFromMap($result['regional_average_record'])
         );
     }
 }
